02bc05e30305baa3d751f3b4158f7bf445d5f77e
Paul Vidler <45616638+PaulVidler@users.noreply.github.com>
2021-12-15 18:05:50 +1000

    First Commit

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,118 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+// mui
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import TextField from "@material-ui/core/TextField";
+import Container from "@material-ui/core/Container";
+import KeyboardArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
+import {
+  FormControlLabel,
+  makeStyles,
+  FormControl,
+  FormLabel,
+} from "@material-ui/core";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+// styling
+const useStyles = makeStyles({
+  // btn: {
+  //   fontSize: 60,
+  //   backgroundColor: "violet",
+  //   "&:hover": {
+  //     backgroundColor: "blue",
+  //   }
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+});
 
 export default function Create() {
+  const classes = useStyles();
+  const history = useHistory();
+  const [title, setTitle] = useState("");
+  const [details, setDetails] = useState("");
+  const [detailsError, setDetailsError] = useState(false);
+  const [titleError, setTitleError] = useState(false);
+  const [category, setCategory] = useState("todos");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && details) {
+      // console.log(title, details, category);
+      fetch("http://localhost:8000/notes/", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify({ title, details, category }),
+      }).then(() => history.push("/"));
+    }
+    title ? setTitleError(false) : setTitleError(true);
+    details ? setDetailsError(false) : setDetailsError(true);
+  };
+
   return (
-    <div>
-      Create page
-    </div>
-  )
+    <Container>
+      <Typography
+        // className={classes.title}
+        variant="h6"
+        component="h2"
+        gutterBottom
+        color="textSecondary"
+      >
+        Create a new note
+      </Typography>
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <TextField
+          onChange={(e) => setTitle(e.target.value)}
+          className={classes.field}
+          label="Note title"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          error={titleError}
+        />
+        <TextField
+          onChange={(e) => setDetails(e.target.value)}
+          className={classes.field}
+          label="Details"
+          variant="outlined"
+          color="secondary"
+          fullWidth
+          multiline
+          rows={4}
+          error={detailsError}
+        />
+        <FormControl className={classes.field}>
+          <FormLabel>Note Category</FormLabel>
+          <RadioGroup
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
+            <FormControlLabel value="money" control={<Radio />} label="Money" />
+            <FormControlLabel value="todos" control={<Radio />} label="ToDos" />
+            <FormControlLabel
+              value="reminders"
+              control={<Radio />}
+              label="Reminders"
+            />
+            <FormControlLabel value="work" control={<Radio />} label="Work" />
+
+            {/* <Radio value="bye" /> */}
+          </RadioGroup>
+        </FormControl>
+        <Button
+          variant="contained"
+          type="submit"
+          color="secondary"
+          endIcon={<KeyboardArrowRightIcon />}
+        >
+          Submit
+        </Button>
+      </form>
+    </Container>
+  );
 }
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+// components
+import NoteCard from "../Components/NoteCard";
+// mui
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { Container } from "@material-ui/core";
 
 export default function Notes() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/notes/")
+      .then((res) => res.json())
+      .then((data) => setNotes(data));
+  }, []);
+
+  const handleDelete = async (id) => {
+    await fetch("http://localhost:8000/notes/" + id, {
+      method: "DELETE",
+    });
+
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
+
   return (
-    <div>
-      Notes page
-    </div>
-  )
+    <Container>
+      <Grid container spacing={3}>
+        {notes.map((note) => (
+          <Grid item xs={12} sm={6} md={3} lg={3} key={note.id}>
+            {/* <Paper>{note.title}</Paper> */}
+            <NoteCard note={note} handleDelete={handleDelete} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
 }
